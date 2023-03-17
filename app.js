49474449cc231fbbb67545d9b4bd90600c12e512
Md Aftab <mdaftabn97@gmail.com>
2023-03-17 13:14:53 +0530

    bug fix

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-const = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 
